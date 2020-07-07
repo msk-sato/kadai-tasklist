130bc5280c104d2ef1c9386aa7d932c41e7410b1
misaki.sato <2015outoc@gmail.com>
2020-07-07 13:55:59 +0000

    ログイン認証機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in #only: [:show, :edit, :update, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
@@ -12,8 +13,7 @@ class TasksController < ApplicationController
   end
   
   def create
-     @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'タスク が正常に投稿されました'
       redirect_to @task
