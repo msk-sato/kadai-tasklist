3ac80ba5c6a76975e58ccc348225ef7d2b81fa96
misaki.sato <2015outoc@gmail.com>
2020-07-08 11:20:04 +0000

    ログイン認証再修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in #only: [:show, :edit, :update, :destroy]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in, only: [:index, :show, :update, :create, :destroy]
+  before_action :set_task
   
   def index
     if logged_in?
@@ -55,13 +54,6 @@ class TasksController < ApplicationController
   end
   
   def set_task
-    @task = Task.find(params[:id])
-  end
-  
-  def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
-    unless @task
-      redirect_to root_url
-    end
   end
 end
