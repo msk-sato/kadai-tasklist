15670e7a4f1ad6b4867a9d5c17bde01a1ccd837b
misaki.sato <2015outoc@gmail.com>
2020-07-09 13:32:50 +0000

    TaskController修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :new, :edit, :update, :create, :destroy]
+  before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :update, :edit, :destroy]
   
   def index
